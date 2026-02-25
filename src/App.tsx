26a64dde0c9b284be96e0170c5f10631daae8fe2
kpollatbs905 <kpollatbs@gmail.com>
2026-02-25 16:11:32 +0700

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: "40px", fontFamily: "sans-serif" }}>
+      <h1>ระบบรับสมัครนักเรียน</h1>
+      <p>Tha-bo Admission System 🚀</p>
+    </div>
+  )
+}
+
+export default App
